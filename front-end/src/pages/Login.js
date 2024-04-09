68906945d1546c622e24144ea0fdcae1daf87d1a
EyadKamash <kamasheyad@gmail.com>
2024-04-09 16:31:13 +0200

    Login redirects to homepage

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import axios from "axios";
 import "../CSS/FormPages.css";
 import SignInImage from "../assets/sign_in_up.png";
@@ -7,6 +7,7 @@ import SignInImage from "../assets/sign_in_up.png";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [redirect, setRedirect] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -16,6 +17,7 @@ const Login = () => {
         password,
       });
       alert("Login Successful");
+      setRedirect(true);
       console.log(response.data); // Log the response from the backend
       // Optionally, redirect the user to another page
     } catch (error) {
@@ -26,51 +28,55 @@ const Login = () => {
 
   return (
     <div>
-      <div className="sign-up-container">
-        <div className="form-container">
-          <div className="form-content">
-            <h1 className="form-title text-[#00df9a] ">
-              YOUR JOURNEY STARTS HERE
-            </h1>
-            <form onSubmit={handleSubmit} className="form">
-              <div className="input-group">
-                <label className="label">Email</label>
-                <input
-                  className="input"
-                  type="email"
-                  required
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="input-group">
-                <label className="label">Password</label>
-                <input
-                  className="input"
-                  type="password"
-                  required
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="flex">
-                <button
-                  className="bg-[#00df9a] w-[200px] rounded-md font-bold my-6 mx-auto py-3 text-black"
-                  type="submit"
-                >
-                  Log In
-                </button>
-              </div>
-            </form>
-            <p className="sign-in-link text-center ">
-              New User? <Link to="/register">Sign Up</Link>
-            </p>
+      {redirect ? (
+        <Navigate to="/" /> // Redirect to homepage if redirect state is true
+      ) : (
+        <div className="sign-up-container">
+          <div className="form-container">
+            <div className="form-content">
+              <h1 className="form-title text-[#00df9a] ">
+                YOUR JOURNEY STARTS HERE
+              </h1>
+              <form onSubmit={handleSubmit} className="form">
+                <div className="input-group">
+                  <label className="label">Email</label>
+                  <input
+                    className="input"
+                    type="email"
+                    required
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="input-group">
+                  <label className="label">Password</label>
+                  <input
+                    className="input"
+                    type="password"
+                    required
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <div className="flex">
+                  <button
+                    className="bg-[#00df9a] w-[200px] rounded-md font-bold my-6 mx-auto py-3 text-black"
+                    type="submit"
+                  >
+                    Log In
+                  </button>
+                </div>
+              </form>
+              <p className="sign-in-link text-center ">
+                New User? <Link to="/register">Sign Up</Link>
+              </p>
+            </div>
+            <img className="sign-up-image" src={SignInImage} alt="Sign In" />
           </div>
-          <img className="sign-up-image" src={SignInImage} alt="Sign In" />
         </div>
-      </div>
+      )}
     </div>
   );
 };
