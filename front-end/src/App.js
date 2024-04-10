4abcf0304dca61d41c5081d0b5a281d11e53e262
EyadKamash <kamasheyad@gmail.com>
2024-04-10 03:04:09 +0200

    client interface v1

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Layout from "./Layout";
 import Register from "./pages/Register";
 import axios from "axios";
+import ClientInterface from "./pages/ClientInterface";
 
 axios.defaults.baseURL = "http://localhost:4000";
 
@@ -15,6 +16,7 @@ function App() {
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/clienthome" element={<ClientInterface />} />
       </Route>
     </Routes>
   );
---
+++ front-end/src/pages/ClientInterface.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ClientInterface() {
+  return <div>ClientInterface</div>;
+}
+
+export default ClientInterface;
