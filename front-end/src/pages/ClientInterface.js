fb38f1340125f514d50a1c13608d2ee4b90fc631
EyadKamash <kamasheyad@gmail.com>
2024-04-25 19:13:30 +0200

    client update #1

--- front-end/src/pages/ClientInterface.js
+++ front-end/src/pages/ClientInterface.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Jobpost from "../components/Jobpost";
 import { UserContext } from "../UserContext";
+import OptionsBar from "../components/OptionsBar";
+import DBoardContent from "../Functions/DBoardContent";
+import Jobpost from "../components/Jobpost";
 
 function ClientInterface() {
   const { user } = useContext(UserContext);
@@ -12,7 +14,22 @@ function ClientInterface() {
   const [filter, setFilter] = useState("");
   const [showRemote, setShowRemote] = useState(false);
   const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedOption, setSelectedOption] = useState("Jobs");
   const navigate = useNavigate();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     if (!user) {
@@ -72,47 +89,78 @@ function ClientInterface() {
     setFilteredJobs(filtered);
   };
 
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
-    <div className="flex flex-col lg:flex-row">
-      <div className="lg:w-1/4 p-4">
-        <h1 className="text-lg font-bold mb-2">Filters</h1>
-        <input
-          type="text"
-          placeholder="Search by job title"
-          value={filter}
-          onChange={handleFilterChange}
-          className="border border-gray-300 rounded-lg px-3 py-1 w-full mb-4"
+    <div className="flex h-screen overflow-hidden">
+      <div className="w-48 bg-black-200 flex-shrink-0">
+        <OptionsBar
+          selectedOption={selectedOption}
+          handleOptionSelect={handleOptionSelect}
         />
-        <label className="flex items-center mb-2">
-          <input
-            type="checkbox"
-            checked={showRemote}
-            onChange={handleRemoteChange}
-            className="mr-2"
-          />
-          Remote Jobs
-        </label>
-        <select
-          value={selectedCountry}
-          onChange={handleCountryChange}
-          className="border border-gray-300 rounded-lg px-3 py-1 w-full"
-        >
-          <option value="">All Countries</option>
-          {/* Add your country options here */}
-        </select>
       </div>
-      <div className="lg:w-3/4 p-4">
-        <h1 className="text-white text-right mr-11">HELLO, {firstname}</h1>
-        <button
-          className="bg-[#ff5050] w-[200px] rounded-md font-bold my-6 mx-auto py-3 text-black"
-          onClick={handleLogout}
+      <div className="flex-1 flex flex-col">
+        <div className="bg-white flex flex-col flex-1 px-4 py-4 md:px-8 md:py-8">
+          {selectedOption === "Jobs" && (
+            <>
+              <h1 className="text-lg font-bold mb-2">Filters</h1>
+              <input
+                type="text"
+                placeholder="Search by job title"
+                value={filter}
+                onChange={handleFilterChange}
+                className="border border-gray-300 rounded-lg px-3 py-1 w-full mb-4"
+              />
+              <label className="flex items-center mb-2">
+                <input
+                  type="checkbox"
+                  checked={showRemote}
+                  onChange={handleRemoteChange}
+                  className="mr-2"
+                />
+                Remote Jobs
+              </label>
+              <select
+                value={selectedCountry}
+                onChange={handleCountryChange}
+                className="border border-gray-300 rounded-lg px-3 py-1 w-full"
+              >
+                <option value="">All Countries</option>
+                {/* Add your country options here */}
+              </select>
+              <div className="flex flex-col space-y-4 p-12">
+                {filteredJobs.map((job) => (
+                  <Jobpost key={job._id} job={job} className="m-2" />
+                ))}
+              </div>
+            </>
+          )}
+          {selectedOption !== "Jobs" && (
+            <DBoardContent selectedOption={selectedOption} />
+          )}
+        </div>
+        <div
+          className={`flex items-center  ${
+            isMobile ? "flex-col" : "flex-row"
+          } space-y-4 p-4`}
         >
-          Logout
-        </button>
-        <div className="text-white">
-          {filteredJobs.map((job) => (
-            <Jobpost key={job._id} job={job} />
-          ))}
+          <h1
+            className={`text-lg font-bold text-white ${
+              isMobile ? "text-center" : ""
+            }`}
+          >
+            Hello, {firstname}
+          </h1>
+          <button
+            className={`p-2 bg-red-500 text-white rounded ml-10 ${
+              isMobile ? "w-full" : ""
+            }`}
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
         </div>
       </div>
     </div>
